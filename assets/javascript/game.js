19647cc6e6fe8125fd755f77696c74de0038b0e6
Jean Peel <Jean.Peel.246@gmail.com>
2020-01-26 15:08:14 -0700

    set up restart and collect btns

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,23 +7,20 @@ var blueOvalClk= document.getElementById('blue-oval');
 var pinkDiamondClk = document.getElementById('pink-diamond');
 var greenHeartClk = document.getElementById('green-heart');
 var gemsLd = document.getElementsByClassName('gem');
-// var restartClk = document.getElementById('restart');
-var submitClk = document.getElementById('submit');
+var restartClk = document.getElementById('restart');
+var collectClk = document.getElementById('collect');
 
 var wins = 0;
 var losses = 0;
 var goalValue = 0;
 var collectedValue =0;
 
-var randomNumber1 = Math.floor(Math.random() * 11 + 1);
-var randomNumber2 = Math.floor(Math.random() * 11 + 1);
-var randomNumber3 = Math.floor(Math.random() * 11 + 1);
-var randomNumber4 = Math.floor(Math.random() * 11 + 1);
+var randomNumber1 = Math.ceil(Math.random() * 12);
+var randomNumber2 = Math.ceil(Math.random() * 12);
+var randomNumber3 = Math.ceil(Math.random() * 12);
+var randomNumber4 = Math.ceil(Math.random() * 12);
 
-var randomNumber5 = Math.floor(Math.random() * 119 + 1);
-
-
-
+var randomNumber5 = Math.floor(Math.random() * 101)+20;
 
         console.log('Purple Circle ' + randomNumber1);
         console.log('Blue Oval ' + randomNumber2);
@@ -33,17 +30,63 @@ var randomNumber5 = Math.floor(Math.random() * 119 + 1);
         console.log('Goal Value ' + randomNumber5);
 
         document.getElementById('blue-oval').addEventListener('click',function(){
-                console.log('Blue Oval has been clicked!')
+        console.log('Blue Oval has been clicked!')
         });
 
 
 $('#restart').click(function(){
         console.log('Restart has been clicked!')
+        restartGame()
 });
 
-// $('#blue-oval').on('click',function(){
-//         console.log('Blue Oval has been clicked!')
-// });
+function restartGame() {
+        var randomNumber1 = Math.ceil(Math.random() * 12);
+        var randomNumber2 = Math.ceil(Math.random() * 12);
+        var randomNumber3 = Math.ceil(Math.random() * 12);
+        var randomNumber4 = Math.ceil(Math.random() * 12);
+
+        var randomNumber5 = Math.floor(Math.random() * 101)+20;
+
+        console.log(randomNumber5 + " Goal Value reset")  
+
+        console.log(randomNumber1 + " Purple Value reset");
+        console.log(randomNumber2 + " Blue Value reset");
+        console.log(randomNumber3 + " Pink Value reset");
+        console.log(randomNumber4 + " Green Value reset");
+
+        var wins = 0;
+        var losses = 0;
+        var goalValue = 0;
+        var collectedValue =0;
+
+        console.log("Scores Reset!")
+}
+
+$('#collect').click(function(){
+        console.log('collect has been clicked!')
+        collectScores()
+});
+
+function collectScores() {
+     
+
+        var randomNumber1 = Math.ceil(Math.random() * 12);
+        var randomNumber2 = Math.ceil(Math.random() * 12);
+        var randomNumber3 = Math.ceil(Math.random() * 12);
+        var randomNumber4 = Math.ceil(Math.random() * 12);
+
+        var randomNumber5 = Math.floor(Math.random() * 101)+20;
+
+        console.log(randomNumber5 + " Goal Value collected")  
+
+        console.log(randomNumber1 + " Purple Value collectted");
+        console.log(randomNumber2 + " Blue Value collectted");
+        console.log(randomNumber3 + " Pink Value collectted");
+        console.log(randomNumber4 + " Green Value collectted");
+}
+
+
+
 
 
 
